9ac5d3301044bb6368c8b2cbafda0ec6db35be0f
yicat <wallergager@gmail.com>
2018-09-22 18:56:51 +0800

    chore: 搭建脚手架

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.css";
+declare module "*.less";
+
+declare module "rsuite";
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './index.less';
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import 'rsuite/styles/less/index.less';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
